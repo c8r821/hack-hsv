33df0432a6930b0475d01a371bf0118f23c9ca9d
Nitroxko <qjohn808@gmail.com>
2018-02-24 11:56:09 -0600

    crush function

--- submarine.js
+++ submarine.js
@@ -4,3 +4,7 @@ function crush(exPressure, radius, yieldStrength, thickness ) {
        return True;
    }
 }
+function calcPressure(depth){
+    pressure= (depth*0.003063)+0.101325
+    return pressure
+}
