798dccd8907b31cc0efad68a048da7de87d72d8d
Jasmine Le <le.jasmine.ncths@gmail.com>
2018-02-24 16:02:00 -0600

    FIles

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,15 @@ const router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+    res.render('index', { title: 'Express' });
+});
+
+router.get('/oxygen', function(req, res, next) {
+    res.render('oxygen', { title: 'Express' });
+});
+
+router.get('/about', function(req, res, next) {
+    res.render('about', { title: 'Express' });
 });
 
 module.exports = router;
