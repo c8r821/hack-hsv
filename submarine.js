eafbe7336caa07e22a128e40a86b648e74a5cfb7
Nitroxko <qjohn808@gmail.com>
2018-02-24 12:15:54 -0600

    Added Most Backend Functions

--- submarine.js
+++ submarine.js
@@ -1,10 +1,30 @@
 function crush(exPressure, radius, yieldStrength, thickness ) {
    temp=(exPressure*radius)/(2*yieldStrength);
    if (thickness >= temp) {
-       return True;
+       return true;
    }
 }
 function calcPressure(depth){
-    pressure= (depth*0.003063)+0.101325
-    return pressure
+    pressure= (depth*0.003063)+0.101325;
+    return pressure;
+
 }
+function calcDepth(pressure) {
+    depth=(pressure-0.101325)/0.003063;
+    return depth
+}
+function warning(exPressure, radius, yieldStrength, thickness ){
+    for (i=calcDepth(exPressure);i<calcDepth(exPressure)+10;i++){
+        if(crush(calcDepth(i),radius,yieldStrength,thickness)){
+            return true
+        }
+    }
+
+}
+
+modules.export = {
+    crush,
+    calcDepth,
+    calcPressure,
+    warning
+};
