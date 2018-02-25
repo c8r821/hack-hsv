4e9d91dede9088652eadbf2f14d0e078e661003d
Carter Grimmeisen <cpgrimmeisen@gmail.com>
2018-02-24 19:27:27 -0600

    Moving Submarine and depth.

---
+++ public/javascripts/subMath.js
@@ -0,0 +1,24 @@
+const length=180;
+function crush(depth, radius, yieldStrength, thickness ) {
+   temp=Math.sqrt(calcPressure(depth) * 72.5 * radius) / (1.5 * yieldStrength);
+   console.log("min: ", temp, "real: ", thickness);
+   return (thickness < temp);
+}
+function calcPressure(depth){
+    return (depth*0.003063)+0.101325;
+}
+function calcDepth(pressure) {
+    return (pressure-0.101325)/0.003063;
+}
+function warning(depth, radius, yieldStrength, thickness){
+        if(crush(depth+5,radius,yieldStrength,thickness)) {
+             return true
+        }
+}
+
+function pressureOnHull(exPressure,radius){
+    area=2*Math.PI*radius*length+2*Math.PI*radius**2;
+    areaInMeters=area/3.28;
+    return areaInMeters*exPressure;
+
+}
