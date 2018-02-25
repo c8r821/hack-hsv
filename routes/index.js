bf8b1905866130398324ba356be1619585dc5739
Marcus Jefferson <marcusxjefferson@gmail.com>
2018-02-24 18:10:53 -0600

    more stuff

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    res.render('index', { title: 'Express' });
+    res.render('index', { title: 'Express', styling: "min-height: 715vh; background-color: #91d5ff; background-image: url('/images/background.svg'); background-size: contain; background-position: 0 50vh; background-repeat: no-repeat"});
 });
 
 router.get('/levels', function(req, res, next) {
