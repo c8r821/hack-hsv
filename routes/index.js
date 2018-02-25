f8ad38e98eb01f799192dd756053717cdd66c05e
Carter Grimmeisen <cpgrimmeisen@gmail.com>
2018-02-24 20:22:44 -0600

    Tabs now show active page

--- routes/index.js
+++ routes/index.js
@@ -6,19 +6,19 @@ const schemas = require('../database/schemas');
 router.get('/', async function(req, res, next) {
     let materials = await schemas.materials.find();
     let submarines = await schemas.submarines.find();
-    res.render('index', { title: 'Express', materials, submarines, styling: "min-height: 750vh; background-color: #91d5ff; background-image: url('/images/background.svg'); background-size: contain; background-position: 0 50vh; background-repeat: no-repeat"});
+    res.render('index', { title: 'Home', materials, submarines, styling: "min-height: 750vh; background-color: #91d5ff; background-image: url('/images/background.svg'); background-size: contain; background-position: 0 50vh; background-repeat: no-repeat"});
 });
 
 router.get('/levels', function(req, res, next) {
-    res.render('levels', { title: 'Express', styling: "background-color: #91d5ff"});
+    res.render('levels', { title: 'Levels', styling: "background-color: #91d5ff"});
 });
 
 router.get('/about', function(req, res, next) {
-    res.render('about', { title: 'Express' });
+    res.render('about', { title: 'About' });
 });
 
 router.get('/dsc', function(req, res, next) {
-    res.render('dsc', { title: 'Express' });
+    res.render('dsc', { title: 'DSC' });
 });
 
 
