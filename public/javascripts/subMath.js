d1385f88c3c24c483fff6c1477b3a3006a39c250
Nitroxko <qjohn808@gmail.com>
2018-02-26 11:15:57 -0600

    Fixed Warning

--- public/javascripts/subMath.js
+++ public/javascripts/subMath.js
@@ -11,7 +11,7 @@ function calcDepth(pressure) {
     return (pressure-0.101325)/0.003063;
 }
 function warning(depth, radius, yieldStrength, thickness){
-        if(crush(depth+10,radius,yieldStrength,thickness)) {
+        if(crush(depth+100,radius,yieldStrength,thickness)) {
              return true
         }
 }
@@ -22,7 +22,7 @@ function pressureOnHull(exPressure,radius){
     return areaInMeters*exPressure;
 
 }
-function money(radius,thickness){
+function money(radius,thickness,density,pricePerPound){
     volume=Math.PI*length*((radius-(radius-thickness)));
 
 }
