0f5f07d6384b534b8716d7ad3e6411fbe0e2b76c
Nitroxko <qjohn808@gmail.com>
2018-02-26 12:49:06 -0600

    Finished Money function(Returns price)

--- public/javascripts/subMath.js
+++ public/javascripts/subMath.js
@@ -1,14 +1,11 @@
 const length=180;
 function crush(depth, radius, yieldStrength, thickness ) {
-   temp=Math.sqrt(calcPressure(depth) * 72.5 * radius) / (2 * yieldStrength);
+   let temp=Math.sqrt(calcPressure(depth) * 72.5 * radius) / (2 * yieldStrength);
    console.log("min: ", temp, "real: ", thickness);
    return (thickness < temp);
 }
 function calcPressure(depth){
     return (depth*0.003063)+0.101325;
-}
-function calcDepth(pressure) {
-    return (pressure-0.101325)/0.003063;
 }
 function warning(depth, radius, yieldStrength, thickness){
         if(crush(depth+200,radius,yieldStrength,thickness)) {
@@ -17,16 +14,16 @@ function warning(depth, radius, yieldStrength, thickness){
 }
 
 function pressureOnHull(exPressure,radius){
-    area=2*Math.PI*radius*length+2*Math.PI*radius**2;
-    areaInMeters=area/3.28;
+    let area=2*Math.PI*radius*length+2*Math.PI*radius**2;
+    let areaInMeters=area/3.28;
     return areaInMeters*exPressure;
 
 }
 function money(radius,thickness,density,pricePerPound){
-    volume=Math.PI*length*((radius-(radius-thickness)));
-    mass=density*volume;
-    weight=mass*0.00220462;
-    price=mass
+    let volume=Math.PI*(length*30.48)*(((radius*30.48)-((radius*30.48)-(thickness*30.48))));
+    let mass=density*volume;
+    let weight=mass*0.00220462;
+    return weight*pricePerPound;
 
 
 }
