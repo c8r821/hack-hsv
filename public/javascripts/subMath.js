c2df61fa522fbf930e26e0e87244c308e48a7c53
C8r821 <cpgrimmeisen@gmail.com>
2018-03-02 09:23:19 -0600

    Animations on sub, better layout, added presets, and fixed math

--- public/javascripts/subMath.js
+++ public/javascripts/subMath.js
@@ -1,29 +1,24 @@
-const length=180;
-function crush(depth, radius, yieldStrength, thickness ) {
-   let temp=Math.sqrt(calcPressure(depth) * 72.5 * radius) / (2 * yieldStrength);
-   console.log("min: ", temp, "real: ", thickness);
-   return (thickness < temp);
-}
-function calcPressure(depth){
-    return (depth*0.003063)+0.101325;
-}
-function warning(depth, radius, yieldStrength, thickness){
-        if(crush(depth+200,radius,yieldStrength,thickness)) {
-             return true
-        }
+const length = 180;
+
+function crush(depth, radius, yieldStrength, thickness) {
+    let temp = (calcPressure(depth) * (radius * 0.3048 /* Feet->Meters */)) / (2 * yieldStrength);
+    return (thickness * 0.3048) < temp;
 }
 
-function pressureOnHull(exPressure,radius){
-    let area=2*Math.PI*radius*length+2*Math.PI*radius**2;
-    let areaInMeters=area/3.28;
-    return areaInMeters*exPressure;
+function calcPressure(depth) {
+    return depth * 0.00299;
+}
+
+function pressureOnHull(exPressure, radius) {
+    let area = 2 * Math.PI * radius * length + 2 * Math.PI * radius ** 2;
+    let areaInMeters = area / 3.28;
+    return areaInMeters * exPressure;
 
 }
-function money(radius,thickness,density,pricePerPound){
-    let volume=Math.PI*(length*30.48)*(((radius*30.48)-((radius*30.48)-(thickness*30.48))));
-    let mass=density*volume;
-    let weight=mass*0.00220462;
-    return weight*pricePerPound;
 
-
+function money(radius, thickness, density, pricePerPound) {
+    let volume = Math.PI * (length * 30.48) * (((radius * 30.48) - ((radius * 30.48) - (thickness * 30.48))));
+    let mass = density * volume;
+    let weight = mass * 0.00220462;
+    return weight * pricePerPound;
 }
--- public/javascripts/submarine.js
+++ public/javascripts/submarine.js
@@ -7,12 +7,12 @@ $(function() {
         material = $(this).val();
     });
     $('[name=hthickness]').change(function() {
-        hthickness = +$(this).val() * 0.0018 + 0.02;
+        hthickness = +$(this).val() * (0.01 / 8) + 0.1;
         $('#hthickness').text(parseFloat(Math.round(hthickness * 100) / 100).toFixed(2) + " ft");
     });
     $('[name=radius]').change(function() {
-        radius = +$(this).val() * 0.22 + 8;
-        $('#submarine').css('width', (+$(this).val() * 0.4115 + 38.85).toString() + "vw");
+        radius = +$(this).val() * 0.18 + 7;
+        $('#submarine svg').css('width', (+$(this).val() * 0.3 + 38.85).toString() + "vw");
         $('#radius').text(parseFloat(Math.round(radius * 100) / 100).toFixed(2) + " ft");
     });
 
@@ -21,53 +21,68 @@ $(function() {
     });
     let hasbeendead = 0;
     let windowHeight = $(window).height(), pageHeight = $('body').height();
-    // init controller
-    let controller = new ScrollMagic.Controller();
-    // create a scene
-    new ScrollMagic.Scene({
-        offset: windowHeight * 0.5		// start this scene after scrolling for 50px
-    })
-        .setPin("#submarine") // pins the element for the the scene's duration
-        .addTo(controller); // assign the scene to the controller
 
+    $("#submarine").sticky()
+    
+    let warningDepth = 0;
     $(window).scroll(function() {
-        if ($(window).scrollTop() > windowHeight * 1.2) {
+        if ($(window).scrollTop() > windowHeight * 0.5) {
             let scrollCenter = $(window).scrollTop() - windowHeight / 2;
-            let depth = scrollCenter / pageHeight * 6000 - windowHeight * 0.3;
+            let depth = (scrollCenter - windowHeight * 0.5) / pageHeight * 1300;
             $('#depth').text(parseFloat(Math.round(depth * 100) / 100).toFixed(2).toString() + " ft");
             $('#pressure').text(parseFloat(Math.round(calcPressure(depth) * 100) / 100).toFixed(2).toString() + " MPa");
             $('#hpressure').text(parseFloat(Math.round(pressureOnHull(calcPressure(depth), radius) * 100) / 100).toFixed(2).toString() + " N");
-            if (warning(depth, radius, material, hthickness)) {
-                $('#gonnaDie').show();
+
+
+            if (crush(depth + 500, radius, +material, hthickness)) {
+                dangerDepth = depth;
+                $('#gonnaDie').addClass('blink');
+                $('#submarine svg').addClass("shake-little").addClass('shake-constant');                
+            } else if (crush(depth + 1000, radius, +material, hthickness)) {
+                warningDepth = depth;
+                $('#gonnaDie').removeClass('blink');                                    
+                $('#gonnaDie').css('visibility', 'visible');
+                if (dangerDepth - depth >= 50) {
+                    dangerDepth = 0;
+                    $('#submarine svg').removeClass("shake-little").removeClass('shake-constant');
+                }
             } else {
-                $('#gonnaDie').hide();
+                $('#gonnaDie').css('visibility', 'hidden');
             }
 
             if (crush(depth, radius, +material, hthickness)) {
                 if (!hasbeendead) {
+                    // $('#submarine svg').fadeOut();
                     $(".modal").addClass("is-active");
                     hasbeendead = 1;
                 }
             } else {
                 hasbeendead = 0;
+                $(".modal").removeClass("is-active");
             }
             
             $(".modal-close").click(function() {
                 $(".modal").removeClass("is-active");
             });
-            // console.log(material);
-            // if (crush(depth, radius, +material, hthickness))
-            //     console.log('deded');
-            // else
-            //     console.log('not deded');
-            // console.log("PX:", scroll
-            // Center, "Depth", scrollCenter / pageHeight * 4000 - windowHeight * 0.3, "feet");
         } else {
-            $('#gonnaDie').hide();
+            $(".modal").removeClass("is-active");            
+            $('#gonnaDie').css('visibility', 'hidden');
             $('#depth').text('0.0 ft');
-            $('#pressure').text('0.0 ft');
-            $('#hpressure').text('0.0 ft');
+            $('#pressure').text('0.0 MPa');
+            $('#hpressure').text('0.0 N');
             hasbeendead = 0;
         }
-    })
+    });
+    $("#preSet").click(function() {
+        let sub = JSON.parse($("#preset").val());
+        $('#material').val(sub.material.yieldStrength);
+
+        $('[name=hthickness]').val(sub.hullThickness * (8 / 0.01));
+        // console.log(-(sub.hullThickness * (8 / 0.01));
+        $('[name=radius]').val((sub.radius - 7) / 0.18);
+
+        ["#material", "[name=hthickness]", "[name=radius]"].forEach((ident) => {
+            $(ident).trigger('change');
+        });
+    });
 });
